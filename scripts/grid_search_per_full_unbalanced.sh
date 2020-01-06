1e8d075f883b52a83405ba1aada425238d14c4d7
Dream User <dream_user@gtlpc119.georgiatech-metz.fr>
2020-01-06 17:59:33 +0100

    fixed typos and forgotten arguments

--- scripts/grid_search_per_full_unbalanced.sh
+++ scripts/grid_search_per_full_unbalanced.sh
@@ -9,14 +9,14 @@ run_max=4
 
 # TRAINING PARAMETERS
 iterations=30000
-expererience_replay=2000
+per_refresh=3000
 used_points=12
 pred_points=1
 batch_size=64
 batch_test_size=10000
 batch_val_size=10000
 drop_rate=0.1
-model= MLP_LRELU_d32_d32
+model="MLP_LRELU_d32_d32"
 learning_rate=0.005
 traj_length=20
 
@@ -29,12 +29,12 @@ python_script="$root/networks/run_training.py"
 
 # OUTPUT
 output_root="/cs-share/dream/RSS-2020-Priorization"
-to_file="$output_root/gridsearch_per_full_random.txt"
-save_suffix="$output_root/results/GS/PER_full_random"
-tb_suffix="$output_root/tensorboard/GS/PER_full_random"
+to_file="$output_root/gridsearch_per_full_unbalanced.txt"
+save_suffix="$output_root/results/GS/PER_full_unbalanced"
+tb_suffix="$output_root/tensorboard/GS/PER_full_unbalanced"
 
 # INIT
-mkdir -p $output_suffix
+mkdir -p $output_root
 
 for alpha in $(seq $alpha_min $alpha_max)
 do
@@ -42,8 +42,8 @@ do
 	do
 		for run in $(seq 0 $run_max)
 		do
-			mkdir -p ${save_suffix}/0{$alpha}_0${beta}/r{$run}
-			echo python3 ${python_script} --train_data ${training_set} --val_set ${validation_set} --test_set ${test_set} --batch_size ${batch_size} --val_batch_size ${batch_val_size} -- test_batch_size ${batch_test_size} --input_dim 5 --output_dim 3 --dropout ${drop_rate} --model ${model} --learning_rate ${learning_rate} --timestamp_idx 0 --output ${save_suffix}/0{$alpha}_0${beta}/r{$run} --tb_dir ${tb_suffix} --tb_log_name 0{$alpha}_0${beta}-r{$run} --reader_mode classic --priorization PER --trajectory_length ${traj_lenght} --alpha 0.${alpha} --beta 0.${beta} >> ${to_file}
+			mkdir -p ${save_suffix}/0${alpha}_0${beta}/r${run}
+			echo python3 ${python_script} --train_data ${training_set} --val_data ${validation_set} --test_data ${test_set} --batch_size ${batch_size} --val_batch_size ${batch_val_size} --test_batch_size ${batch_test_size} --input_dim 5 --output_dim 3 --dropout ${drop_rate} --model ${model} --learning_rate ${learning_rate} --timestamp_idx 0 --output ${save_suffix}/0${alpha}_0${beta}/r${run} --tb_dir ${tb_suffix} --tb_log_name 0${alpha}_0${beta}-r${run} --reader_mode classic --priorization PER --trajectory_length ${traj_length} --per_refresh_rate ${per_refresh} --update_batchsize 50000 --alpha 0.${alpha} --beta 0.${beta} --max_iterations ${iterations} >> ${to_file}
 		done
 	done
 done
